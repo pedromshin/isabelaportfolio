9dff3864f378ba283718221f6bca9c9ab8ebe282
pedromshin <pedromaschio.shin@gmail.com>
2022-05-20 03:34:08 -0300

    adds images on home

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import styles from "./Home.module.scss";
+
+import bannerPic1 from "./images/rect.png";
 
 const Home: NextPage = () => {
   return (
@@ -12,23 +15,45 @@ const Home: NextPage = () => {
         <link href="http://fonts.cdnfonts.com/css/avegra" rel="stylesheet" />
       </Head>
 
-      <header className={styles.header}>isabela alves</header>
+      <header className={styles.header}>isabela alves - home</header>
       <main className={styles.main}>
         <div className={styles.content}>
-          <div className={styles.divisor} />
           <div className={styles.linkWrapper}>
-            <a href="/sobre-mim" className={styles.link}>
-              sobre mim
-            </a>
-            <a href="/portfolio" className={styles.link}>
-              portfolio
-            </a>
-            <a href="/curriculo" className={styles.link}>
-              curriculo
-            </a>
-            <a href="/contato" className={styles.link}>
-              contato
-            </a>
+            <div className={styles.divisor} />
+            <div className={styles.linkList}>
+              <a href="/sobre-mim" className={styles.link}>
+                sobre mim
+              </a>
+              <a href="/portfolio" className={styles.link}>
+                portfolio
+              </a>
+              <a href="/curriculo" className={styles.link}>
+                curriculo
+              </a>
+              <a href="/contato" className={styles.link}>
+                contato
+              </a>
+            </div>
+          </div>
+          <div className={styles.imageWrapper}>
+            <div className={styles.image1}>
+              <Image
+                alt="banner image"
+                src={bannerPic1}
+                layout="fixed"
+                width={350}
+                height={440}
+              />
+            </div>
+            <div className={styles.image2}>
+              <Image
+                alt="banner image"
+                src={bannerPic1}
+                layout="fixed"
+                width={350}
+                height={440}
+              />
+            </div>
           </div>
         </div>
         <footer className={styles.footer}>
