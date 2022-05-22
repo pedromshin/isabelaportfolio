aba7ce2daaacb49fa07f6be1ca72721ebce5b5b5
pedromshin <pedromaschio.shin@gmail.com>
2022-05-21 23:23:29 -0300

    meta tags

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -5,12 +5,21 @@ export default function Document() {
     <Html>
       <Head>
         <link href="https://fonts.cdnfonts.com/css/avegra" rel="stylesheet" />
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap"
           rel="stylesheet"
-        ></link>
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+        <meta
+          name="description"
+          content="Personal portfolio of Isabela Alves"
+        />
+        <link rel="canonical" href="https://www.isabelaalves.tk" />
       </Head>
       <body>
         <Main />
